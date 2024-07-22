f3df0b8726cb22eb229ed4745ab11cc510e97db4
guohao <3209949883@qq.com>
2024-07-22 13:31:08 +0800

    7.22 初始化项目

---
+++ src/test/java/com/zuiyou/ZuiYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuiyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuiYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
